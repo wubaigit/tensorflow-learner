417629c28cc340dc38f0e3fb0965056e927e185b
wubaigit <wubai312@163.com>
2019-03-20 13:14:50 +0800

    fix: fix bug when cut file to another dir

--- cutPicture/cut_picture.py
+++ cutPicture/cut_picture.py
@@ -1,10 +1,14 @@
 import os
+import shutil
 from PIL import Image
 
-
-def cut_picture(path, output_dir, output_size):
+def cut_picture(path, output_size):
     (filepath, tempfilename) = os.path.split(path)
     (filename, extension) = os.path.splitext(tempfilename)
+
+    current_path = os.path.dirname(path)
+    print(filepath)
+
     img = Image.open(path)
     width = img.size[0]
     height = img.size[1]
@@ -23,23 +27,38 @@ def cut_picture(path, output_dir, output_size):
             right = x_position + output_size
             bottom = y_position + output_size
             output_img = img.crop((left, top, right, bottom))
-            output_img.save(os.path.join(
-                output_dir, './%s-%s.%s' % (filename, i, extension)))
+            output_img.save(os.path.join(current_path, './%s-%s.%s'% (filename, i, extension)))
             x_position += output_size
             i += 1
         y_position += output_size
+    os.remove(path)
 
-
-def walk(input_dir, output_dir, output_size):
-    for lists in os.listdir(input_dir):
-        path = os.path.join(input_dir, lists)
+def walk(output_dir, output_size):
+    for lists in os.listdir(output_dir):
+        path = os.path.join(output_dir, lists)
         if os.path.isfile(path) and not os.path.isdir(path):
-            cut_picture(path, output_dir, output_size)
+            cut_picture(path, output_size)
         if os.path.isdir(path):
-            walk(path, output_dir)
+            walk(path, output_size)
 
 
 input_dir = './input'
 output_dir = './output'
-output_size = 224
-walk(input_dir, output_dir, output_size)
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) >= 4:
+        input_dir = sys.argv[1]
+        output_dir = sys.argv[2]
+        output_size = int(sys.argv[3])
+        print(output_size)
+
+        try:
+            shutil.copytree(input_dir, output_dir)
+        except FileExistsError:
+            print('file exists')
+
+        walk(output_dir, output_size)
+    else:
+        print('argv is less then four')
